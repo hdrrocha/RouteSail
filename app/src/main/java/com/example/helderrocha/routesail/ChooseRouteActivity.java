11bbbbea5b234c93cc4640b4e1c28f1f408b1199
Helder rocha <hedie.strokes@gmail.com>
2017-09-26 21:04:29 -0300

    add spnner route

--- app/src/main/java/com/example/helderrocha/routesail/ChooseRouteActivity.java
+++ app/src/main/java/com/example/helderrocha/routesail/ChooseRouteActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class ChooseRouteActivity extends AppCompatActivity {
-
+    private Spinner spnOpcoes;
+    private ArrayAdapter<String> adpOpcoes;
+    private  ArrayAdapter<String> adpDados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_route);
 
         Button btnCallActivity = (Button) findViewById(R.id.btnNavegar);
-
+        Spinner spnRoute = (Spinner)findViewById(R.id.spnRoute);
         btnCallActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,5 +27,15 @@ public class ChooseRouteActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        adpOpcoes = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpOpcoes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnRoute.setAdapter(adpOpcoes);
+        adpOpcoes.add("Viado");
+        adpOpcoes.add("Bixa Loka");
+        adpOpcoes.add("Maxo Alfa");
+        adpOpcoes.add("Femea");
+        adpDados = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
     }
 }
