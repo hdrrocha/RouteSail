01dddab48d08162a60e9873978d24cfcf79b6a11
Helder rocha <hedie.strokes@gmail.com>
2017-10-23 21:04:00 -0200

    create strucure of calcu

--- app/src/main/java/com/example/helderrocha/routesail/MapsActivity.java
+++ app/src/main/java/com/example/helderrocha/routesail/MapsActivity.java
@@ -49,7 +49,6 @@ import java.util.List;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
-    //    GoogleMap map;
     ArrayList<LatLng> markerPoints;
     private AutoCompleteTextView mEditRotas;
 
@@ -91,79 +90,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-
-
-
     }
 
     private void loadDefaultNos(){
-//        nos = new ArrayList<>();
-//        NoMaritmo noMaritmo = new NoMaritmo();
-//        noMaritmo.setId(1);
-//        noMaritmo.setPosicao(new LatLng(-20,-30));
-//        nos.add(noMaritmo);
-//        noMaritmo = new NoMaritmo();
-//        noMaritmo.setId(2);
-//        noMaritmo.setPosicao(new LatLng(-20,-20));
-//        nos.add(noMaritmo);
-//        noMaritmo = new NoMaritmo();
-//        noMaritmo.setId(3);
-//        noMaritmo.setPosicao(new LatLng(-20,-10));
-//        nos.add(noMaritmo);
-//        noMaritmo = new NoMaritmo();
-//        noMaritmo.setId(4);
-//        noMaritmo.setPosicao(new LatLng(-10,-10));
-//        nos.add(noMaritmo);
-//
-//        mArestaList = new ArrayList<>();
-//        Aresta aresta = new Aresta();
-//        aresta.setNoMaritmo1(nos.get(0));
-//        aresta.setNoMaritmo2(nos.get(1));
-//        aresta.setCorrente(3);
-//        aresta.setVento(3);
-//        aresta.setDistancia(50D);
-//        mArestaList.add(aresta);
-//
-//        aresta = new Aresta();
-//        aresta.setNoMaritmo1(nos.get(0));
-//        aresta.setNoMaritmo2(nos.get(3));
-//        aresta.setCorrente(9);
-//        aresta.setVento(1);
-//        aresta.setDistancia(900D);
-//        mArestaList.add(aresta);
-//
-//        aresta = new Aresta();
-//        aresta.setNoMaritmo1(nos.get(1));
-//        aresta.setNoMaritmo2(nos.get(2));
-//        aresta.setCorrente(5);
-//        aresta.setVento(2);
-//        aresta.setDistancia(30D);
-//        mArestaList.add(aresta);
-//
-//        aresta = new Aresta();
-//        aresta.setNoMaritmo1(nos.get(1));
-//        aresta.setNoMaritmo2(nos.get(3));
-//        aresta.setCorrente(15);
-//        aresta.setVento(3);
-//        aresta.setDistancia(500D);
-//        mArestaList.add(aresta);
-//
-//        aresta = new Aresta();
-//        aresta.setNoMaritmo1(nos.get(2));
-//        aresta.setNoMaritmo2(nos.get(3));
-//        aresta.setCorrente(1);
-//        aresta.setVento(3);
-//        aresta.setDistancia(600D);
-//        mArestaList.add(aresta);
-//
-//        Rotas rotas = new Rotas();
-//        rotas.setId(1);
-//        rotas.setDescricao("Rota teste");
-//        rotas.setArestas(mArestaList);
-//        rotas.setNosMaritmos(nos);
-//
-//
-//        mRotas.add(rotas);
         mRotas.add(CarregadorDeRotas.Rota1());
         mRotas.add(CarregadorDeRotas.Rota2());
         mRotas.add(CarregadorDeRotas.Rota3());
@@ -174,118 +103,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mRotas.add(CarregadorDeRotas.Rota9());
         mRotas.add(CarregadorDeRotas.Rota10());
 
-
     }
-
-
-    private String getDirectionsUrl(LatLng origin, LatLng dest) {
-
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + sensor;
-
-        // Output format
-        String output = "json";
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
-
-
-        return url;
-    }
-
-
-    /**
-     * A method to download json data from url
-     */
-    @SuppressLint("LongLogTag")
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Connecting to url
-            urlConnection.connect();
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception while downloading url", e.toString());
-        } finally {
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-
-    // Fetches data from url passed
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        // Downloading data in non-ui thread
-        @Override
-        protected String doInBackground(String... url) {
-
-            // For storing data from web service
-            String data = "";
-
-            try {
-                // Fetching the data from web service
-                data = downloadUrl(url[0]);
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return data;
-        }
-
-        // Executes in UI thread, after the execution of
-        // doInBackground()
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-            // Invokes the thread for parsing the JSON data
-            parserTask.execute(result);
-
-        }
-    }
-
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
-
         map = googleMap;
-        // Initializing
         markerPoints = new ArrayList<LatLng>();
         LatLng santaCatarina = new LatLng(-27.588462, -48.336853);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(santaCatarina));
@@ -296,22 +118,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private void carregarGrafo() {
         map.clear();
         for (NoMaritmo no : nos) {
-            // create marker
-            MarkerOptions marker = new MarkerOptions().position(no.getPosicao()).title("Hello Maps");
-//            Changing marker icon
+            MarkerOptions marker = new MarkerOptions().position(no.getPosicao()).title(no.getmLocal());
             if(no.getmIcon()!= 0){
                 marker.icon(BitmapDescriptorFactory.fromResource(no.getmIcon()));
             }else {
                 marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.no));
             }
 
-
-
-
-
             map.addMarker(marker);
 
         }
+
         for (Aresta aresta : mArestaList) {
             PolylineOptions polylineOptions = new PolylineOptions();
             switch (aresta.getVento()) {
@@ -332,67 +149,50 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-
-    /**
-     * A class to parse the Google Places in JSON format
-     */
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try {
-                jObject = new JSONObject(jsonData[0]);
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                // Starts parsing data
-                routes = parser.parse(jObject);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        // Executes in UI thread, after the parsing process
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList<LatLng> points = null;
-            PolylineOptions lineOptions = new PolylineOptions();
-            MarkerOptions markerOptions = new MarkerOptions();
-
-            // Traversing through all the routes
-            for (int i = 0; i < result.size(); i++) {
-                points = new ArrayList<LatLng>();
-                lineOptions = new PolylineOptions();
-
-                // Fetching i-th route
-                List<HashMap<String, String>> path = result.get(i);
-
-                // Fetching all the points in i-th route
-                for (int j = 0; j < path.size(); j++) {
-                    HashMap<String, String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                // Adding all the points in the route to LineOptions
-                lineOptions.addAll(points);
-                lineOptions.width(2);
-                lineOptions.color(Color.RED);
-
+    private void   getPossibilidades(NoMaritmo mNo, Aresta mresta) {
+        //valida se a corrente está a favor
+        if (validaCorrente(mNo, mresta)) {
+            //valida se o vento esta contra
+            if (validaVento(mNo, mresta)) {
+                //pinta linha
+                //faz soma
             }
 
-            // Drawing polyline in the Google Map for the i-th route
-            map.addPolyline(lineOptions);
+        }//valida se a corrente esta a favor
+        else if (validaCorrente(mNo, mresta)) {
+            //valida se o vento esta a favor
+            if (validaVento(mNo, mresta)) {
+                //pinta linha
+                //faz soma
+            }
+
+        }else if(verificaDistancias()){
+            //pinta amenor distancia
         }
     }
 
+    private boolean verificaDistancias() {
+        return true;
+    }
+
+
+    private boolean validaCorrente(NoMaritmo mNo, Aresta mresta) {
+
+        if(mresta.getCorrente() == 1){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean validaVento(NoMaritmo mNo, Aresta mresta) {
+
+        if(mresta.getVento() == 1){
+            return true;
+        }
+        return false;
+    }
+
+    private Double caclulaVelocidadeTempo(Double velBarco,Double velCorrente){
+        return velCorrente + velBarco;
+    }
 }
